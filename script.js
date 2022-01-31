37eafbf7f75866d230dbb8d2713242f004ac22d9
fanendrashelki <fanendrashelki@gmail.com>
2022-01-31 11:59:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+var mySidebar = document.getElementById("mySidebar");
+
+function w3_open() {
+    if (mySidebar.style.display === 'block') {
+        mySidebar.style.display = 'none';
+    } else {
+        mySidebar.style.display = 'block';
+    }
+}
+
+// Close the sidebar with the close button
+function w3_close() {
+    mySidebar.style.display = "none";
+}
+
+
+//slider 
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) { myIndex = 1 }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000); // Change image every 2 seconds
+}
+
+
+
+
+function incrementValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+function decrementValue() {
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('number').value = value;
+}
+
+function increment() {
+    var value = parseInt(document.getElementById('num').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('num').value = value;
+}
+function decrement() {
+    var value = parseInt(document.getElementById('num').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value--;
+    document.getElementById('num').value = value;
+}
